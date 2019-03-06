dce06eca04fbfe9b0df1d275267765781e15b5a1
ygokcebay <ygokcebay@etu.edu.tr>
2019-03-06 23:50:25 +0300

    Whole homework in one commit
    Wrote the TestUtil.java and TestCompute.java classes

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -14,4 +14,35 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+
+  @Test
+  public void test1() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(0);
+    assertTrue("Sent text is : a and mq's size is 0",c.countNumberOfOccurrences("a") == -1);
+  }
+
+  @Test
+  public void test2() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(12);
+    when(mq.contains("a")).thenReturn(false);
+    assertTrue("Sent text is : 'a' and mq does not contain 'a'",c.countNumberOfOccurrences("a") == 0);
+  }
+
+  @Test
+  public void test3() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+
+    when(mq.size()).thenReturn(3);
+    when(mq.contains("a")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("a");
+    when(mq.getAt(1)).thenReturn("a");
+    when(mq.getAt(2)).thenReturn("a");
+
+    assertTrue("Sent text is : a and mq contains a 3 times",c.countNumberOfOccurrences("a") == 3);
+  }
 }
--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
@@ -13,4 +14,27 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
-}
+
+  @Test
+  public void test1() {assertTrue("Length 0   ",!(c.compute())); }
+
+  @Test
+  public void test2() {assertTrue("Length 1   ",!(c.compute(1))); }
+
+  @Test
+  public void test3() {assertTrue("Length 2   ",!(c.compute(1,2))); }
+
+  @Test
+  public void test4() {
+    try {
+      c.compute(1, 2, 0);
+    }catch(RuntimeException e){
+      assertTrue(e.getClass().equals(RuntimeException.class));
+    }
+  }
+
+  @Test
+  public void test5() {assertTrue("Length 3 without 0   ",c.compute(1,2,3)); }
+  @Test
+  public void test6() {assertTrue("Length 3 without 0, false   ",!c.compute(2,2,3));}}
+
